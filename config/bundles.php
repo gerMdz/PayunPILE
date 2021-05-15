efef86c5f2461fa7327935648bacc310d72333ec
Gerardo J. Montivero <gerardo.montivero@gmail.com>
2021-05-14 21:44:29 -0300

    Haciendo mejores mails

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
+    Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
 ];
